68bf7dee9ac0ed7e2507b5b39fbbe8fa385df125
jeanCl loayza <ferloje@hotmail.com>
2022-06-28 18:16:56 -0500

    proyecto con spring security

--- src/main/java/com/idat/MayoServicioPrueba/service/ProductoService.java
+++ src/main/java/com/idat/MayoServicioPrueba/service/ProductoService.java
@@ -2,13 +2,15 @@ package com.idat.MayoServicioPrueba.service;
 
 import java.util.List;
 
+import com.idat.MayoServicioPrueba.dto.ProductoDTORequest;
+import com.idat.MayoServicioPrueba.dto.ProductoDTOResponse;
 import com.idat.MayoServicioPrueba.model.Productos;
 
 public interface ProductoService {
 
-	void guardarProducto(Productos producto);
-	void actualizarProducto(Productos producto);
+	void guardarProducto(ProductoDTORequest producto);
+	void actualizarProducto(ProductoDTORequest producto);
 	void eliminarProducto(Integer id);
-	List <Productos> listarProductos();
-	Productos obtenerProductoId(Integer id);
+	List <ProductoDTOResponse> listarProductos();
+	ProductoDTOResponse obtenerProductoId(Integer id);
 }
